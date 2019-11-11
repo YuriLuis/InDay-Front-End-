e2a5f7ece5252334b8bf5c68f04dfdc17e1f5607
JackMaul <jacksonmmaul@gmail.com>
2019-11-11 19:36:40 -0300

    Receita

--- coreui-free-vue-admin-template-master/src/views/modal/Despesa.vue
+++ coreui-free-vue-admin-template-master/src/views/modal/Despesa.vue
@@ -129,7 +129,76 @@
       v-model="successModal"
       @ok="successModal = false"
       ok-variant="success"
-    ></b-modal>
+    >
+          <b-row>
+        <b-col sm="12">
+          <b-card>
+            <div slot="header">
+              <strong>Lançamento de Receita</strong>
+              <small></small>
+            </div>
+            <b-form-group>
+              <label for="descrição"><strong>Descrição</strong></label>
+              <b-form-input type="text" id="descrição" placeholder="Informe a descrição"></b-form-input>
+            </b-form-group>
+            <b-row>
+              <b-col sm="6">
+                <b-form-group id="valor">
+                  <label for="valor"><strong>Valor</strong></label>
+                  <b-input-group>
+                    <b-input-group-prepend>
+                      <b-input-group-text>
+                        <i class="fa fa-brasil"></i>
+                      </b-input-group-text>
+                    </b-input-group-prepend>
+                    <b-form-input type="text" placeholder="R$ 0.00"></b-form-input>
+                  </b-input-group>
+                </b-form-group>
+              </b-col>
+              <b-col sm="6">
+                <b-form-group>
+                  <label for="data"><strong>Data</strong></label>
+                  <b-form-input type="date" id="data"></b-form-input>
+                </b-form-group>
+              </b-col>
+            </b-row>
+
+            <strong> 
+            <b-form-group  label="Categoria" label-for="Categorias" :label-cols="3">
+              <b-form-select
+                id="basicSelect"
+                :plain="true"
+                :options="['Categorias','Option 1', 'Option 2', 'Option 3']"
+                value="Categorias"
+              ></b-form-select>
+            </b-form-group>
+            </strong>
+            <b-form-group label-for="basicCheckboxes" :label-cols="0">
+              <b-form-checkbox-group
+                stacked
+                id="basicCheckboxes"
+                name="Checkboxes"
+                :plain="true"
+                :checked="[]"
+              >
+                <b-form-checkbox value="1"> <strong>é receita fixa? (mensal) </strong></b-form-checkbox>
+                <b-form-checkbox value="2"> <strong>é um lançamento parcelado?</strong></b-form-checkbox>
+              </b-form-checkbox-group>
+            </b-form-group>
+            <div class="form-actions">
+              <b-row class="align-items-center mt-2">
+                <b-col sm xs="12" class="text-center mt-2">
+                  <b-button type="submit" variant="success" size="sm">Salvar</b-button>
+                </b-col>
+                <b-col sm xs="12" class="text-center mt-2">
+                  <b-button type="button" variant="danger" size="sm">Cancelar</b-button>
+                </b-col>
+              </b-row>
+            </div>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-modal>
     <b-modal
       title="Metas"
       variant="warning"
