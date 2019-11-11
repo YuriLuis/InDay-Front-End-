2150b594ed4d07fb228886806bf68c9d4d65ea86
janquielfelipeto <janquielfelipeto@gmail.com>
2019-11-11 19:00:55 -0300

    tela de login 2 modelos

--- coreui-free-vue-admin-template-master/src/views/pages/Register.vue
+++ coreui-free-vue-admin-template-master/src/views/pages/Register.vue
@@ -6,13 +6,13 @@
           <b-card no-body class="mx-4">
             <b-card-body class="p-4">
               <b-form>
-                <h1>Register</h1>
-                <p class="text-muted">Create your account</p>
+                <h1>Login</h1>
+                <p class="text-muted">Entrar ou Criar conta</p>
                 <b-input-group class="mb-3">
                   <b-input-group-prepend>
                     <b-input-group-text><i class="icon-user"></i></b-input-group-text>
                   </b-input-group-prepend>
-                  <b-form-input type="text" class="form-control" placeholder="Username" autocomplete="username" />
+                  <b-form-input type="text" class="form-control" placeholder="Nome de usuário" autocomplete="Nome de usuário" />
                 </b-input-group>
 
                 <b-input-group class="mb-3">
@@ -26,17 +26,17 @@
                   <b-input-group-prepend>
                     <b-input-group-text><i class="icon-lock"></i></b-input-group-text>
                   </b-input-group-prepend>
-                  <b-form-input type="password" class="form-control" placeholder="Password" autocomplete="new-password" />
+                  <b-form-input type="password" class="form-control" placeholder="Senha" autocomplete="Nova senha" />
                 </b-input-group>
 
                 <b-input-group class="mb-4">
                   <b-input-group-prepend>
                     <b-input-group-text><i class="icon-lock"></i></b-input-group-text>
                   </b-input-group-prepend>
-                  <b-form-input type="password" class="form-control" placeholder="Repeat password" autocomplete="new-password" />
+                  <b-form-input type="password" class="form-control" placeholder="Repetir senha" autocomplete="Repetir senha" />
                 </b-input-group>
 
-                <b-button variant="success" block>Create Account</b-button>
+                <b-button variant="success" block>Criar conta</b-button>
               </b-form>
             </b-card-body>
             <b-card-footer class="p-4">
@@ -45,7 +45,7 @@
                   <b-button block class="btn btn-facebook"><span>facebook</span></b-button>
                 </b-col>
                 <b-col cols="6">
-                  <b-button block class="btn btn-twitter" type="button"><span>twitter</span></b-button>
+                  <b-button block class="btn btn-gmail" type="button"><span>gmail</span></b-button>
                 </b-col>
               </b-row>
             </b-card-footer>
